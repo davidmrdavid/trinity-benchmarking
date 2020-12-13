3054a4212bcace5e5d5801a860feb47ab85fbb33
root <root@node.dmrd-qv87507.orion-pg0.clemson.cloudlab.us>
2020-12-13 14:31:46 -0500

    Change runOps config to run materialized w/ synthesized dataset

---
+++ runOps.py
@@ -0,0 +1,46 @@
+import sys
+import subprocess
+
+CMD = "mx --dynamicimports /compiler,fastr,/tools --cp-sfx ../../mxbuild/dists/jdk1.8/morpheusdsl.jar --jdk jvmci R  --J @'-Xmx220G -da -dsa -agentlib:hprof=cpu=samples' --R.PrintErrorStacktracesToFile=true  --polyglot -f benchmarkRunner.r --args -fpath %s -task %s -outputDir r_raw -mode %s -TR %d -FR %d"
+
+# You need to remove hprof stuff above ^
+
+
+#D = ["movie_metadata.json"]#, 
+#D = ["yelp_metadata.json", "walmart_metadata.json",
+#     "books_metadata.json", "lastfm_metadata.json", "expedia_metadata.json",
+#     "flights_metadata.json"] 
+#M = ["linearRegression","logisticRegression"]#, "kMeansClustering"]
+#D = ["flights_metadata.json", "lastfm_metadata.json"]
+D = ["synthesized.json"]
+M = ["scalarAddition"]
+for d in D:
+
+  d = "./benchparams/" + d
+  for m in M:
+    for TR in range(1,2):#15,16):
+      for FR in range(1,2):
+        #print("RUNNING: ", "TR=",TR, "FR=",FR)
+        #COMMAND = CMD % ("rowWiseSum", "materialized", TR, FR)
+        #pipe = subprocess.Popen(COMMAND, shell=True)
+        #pipe.wait()
+
+        #print("RUNNING: ", "TR=",TR, "FR=",FR)
+        #COMMAND = CMD % ("linearRegression", "trinity", TR, FR)
+        #pipe = subprocess.Popen(COMMAND, shell=True)
+        #pipe.wait()
+
+        #print("RUNNING: ", "TR=",TR, "FR=",FR)
+        #COMMAND = CMD % (d, m, "trinity", TR, FR)
+        #pipe = subprocess.Popen(COMMAND, shell=True)
+        #pipe.wait()
+
+        #print("RUNNING: ", "TR=",TR, "FR=",FR)
+        #COMMAND = CMD % (d, m, "morpheusR", TR, FR)
+        #pipe = subprocess.Popen(COMMAND, shell=True)
+        #pipe.wait()
+
+        print("RUNNING: ", "TR=",TR, "FR=",FR)
+        COMMAND = CMD % (d, m, "materialized", TR, FR)
+        pipe = subprocess.Popen(COMMAND, shell=True)
+        pipe.wait()
